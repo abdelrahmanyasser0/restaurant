c1fd8dd7662df199dd328b7af8b05798165de20e
abdelrahmanyasser0 <abdelrahmanbody775@gmail.com>
2025-08-09 20:02:02 +0300

    build restaurant

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,7 +15,9 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget( MyApp());
 
-    // Verify that our counter starts at 0.
+
+
+    // Verify that our counter starts .
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
